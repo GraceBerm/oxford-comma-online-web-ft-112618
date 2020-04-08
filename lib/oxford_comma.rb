c697de0ec288ed37462412b0eb722aaca97b82b7
Grace Berman <grace.g.berman@gmail.com>
2020-04-08 21:41:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    new_array = array.insert(-2, " and ")
-    new_array.join(", ")
+    array.join(", ")
+    array.insert(-2, " and ")
   end
 end
